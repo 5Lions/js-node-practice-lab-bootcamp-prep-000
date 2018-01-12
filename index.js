10a69dcb8b6dbaa23a36a1542ffd629a6e7922c8
A Lee <ayslee@gmail.com>
2018-01-12 02:18:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "homie"
-var height = "500"
+var height = 74
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) +1)
+console.log(height +1)
